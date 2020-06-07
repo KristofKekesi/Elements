3d6de0b20a7c55ac643ed254c93c5ece06235bd1
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2020-06-07 16:04:46 +0200

    Updated tree

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_chem/widgets/searchResults.dart';
+import 'package:flutter_chem/widgets/landing.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en'),
+        const Locale('hu'),
+      ],
+
+      title: 'Elements',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.orange,
+        bottomSheetTheme:
+            BottomSheetThemeData(backgroundColor: Colors.black.withOpacity(0)),
+      ),
+      home: MyHomePage(title: 'Elements'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void _autoAboutDialog() {
+    showAboutDialog(
+        context: context,
+        applicationVersion: '0.0.1',
+        applicationName: widget.title,
+        applicationLegalese: 'Kristof Kekesi');
+  }
+
+  var lang = "english";
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: landing()
+    );
+  }
+}
---
+++ lib/widgets/landing.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chem/widgets/searchResults.dart';
+
+// ignore: camel_case_types
+class landing extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: EdgeInsets.only(
+                  left: (MediaQuery.of(context).size.height +
+                          MediaQuery.of(context).size.width) /
+                      2 *
+                      .05,
+                  top: 20),
+              child: Row(
+                children: <Widget>[
+                  Image(
+                      image: AssetImage("lib/icons/wrench_500.png"),
+                      height: (MediaQuery.of(context).size.height +
+                              MediaQuery.of(context).size.width) /
+                          2 *
+                          .05,
+                      width: (MediaQuery.of(context).size.height +
+                              MediaQuery.of(context).size.width) /
+                          2 *
+                          .05),
+                  Text(
+                    '  Elements:',
+                    textAlign: TextAlign.left,
+                    style: new TextStyle(
+                        color: Colors.black,
+                        fontSize: (MediaQuery.of(context).size.height +
+                                MediaQuery.of(context).size.width) /
+                            2 *
+                            .095,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => searchResults(),
+                ),
+              );
+            },
+            child: Padding(
+              padding: EdgeInsets.only(
+                  bottom: 20,
+                  right: (MediaQuery.of(context).size.height +
+                      MediaQuery.of(context).size.width) /
+                      2 *
+                      .05),
+              child: Container(
+                width: (MediaQuery.of(context).size.width +
+                    MediaQuery.of(context).size.height) /
+                    2 *
+                    .30,
+                height: (MediaQuery.of(context).size.height +
+                    MediaQuery.of(context).size.width) /
+                    2 *
+                    .13,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Colors.deepOrangeAccent,
+                    Colors.orangeAccent,
+                  ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+                  borderRadius: BorderRadius.circular(
+                      (MediaQuery.of(context).size.height +
+                          MediaQuery.of(context).size.width) /
+                          2 *
+                          .02),
+                ),
+                child:
+                Center(
+                  child:
+                  Text(
+                    "Search",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: (MediaQuery.of(context).size.height +
+                            MediaQuery.of(context).size.width) /
+                            2 *
+                            .06),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
