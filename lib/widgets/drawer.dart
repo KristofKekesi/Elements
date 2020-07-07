b7a87d6a44408e1196f28563a44163c4a3363bc6
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2020-07-07 21:01:46 +0200

    minor fixes

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -131,7 +131,7 @@ class customDrawer extends StatelessWidget {
 void autoAboutDialog(context) {
   showAboutDialog(
       context: context,
-      applicationVersion: '0.0.2',
+      applicationVersion: '0.1.1',
       applicationName: 'Elements',
-      applicationLegalese: 'Kristof Kekesi');
+      applicationLegalese: 'Kristóf Kékesi');
 }
