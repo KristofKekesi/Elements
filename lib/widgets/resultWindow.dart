54958cdd5370cc74b378788545cadb4a884c7624
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2020-07-16 13:13:54 +0200

    Update resultWindow.dart

--- lib/widgets/resultWindow.dart
+++ lib/widgets/resultWindow.dart
@@ -44,6 +44,7 @@ class searchResults extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           //mainAxisAlignment: MainAxisAlignment.center,
