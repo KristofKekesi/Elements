54436d93e26e2c66d8d94170ddf2ff0c1fba3b0c
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2020-09-01 21:25:24 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _MyAppState extends State<MyApp> {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      upportedLocales: [
+      supportedLocales: [
         const Locale('hu', ''),
         const Locale('en', ''),
       ],
