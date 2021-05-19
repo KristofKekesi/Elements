7a3ee0a3580fe9889694360499511b32a7d22e27
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2021-05-19 12:58:39 +0200

    custom in app material color

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,26 @@ import 'package:flutter/material.dart';
 
 import 'package:elements_rework/widgets/build.dart';
 
+MaterialColor createMaterialColor(Color color) {
+  List strengths = <double>[.05];
+  Map swatch = <int, Color>{};
+  final int r = color.red, g = color.green, b = color.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  strengths.forEach((strength) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  });
+  return MaterialColor(color.value, swatch);
+}
+
 class NoScrollGlow extends ScrollBehavior {
   @override
   Widget buildViewportChrome(
@@ -62,7 +82,9 @@ class _MyAppState extends State<MyApp> {
       title: 'Elements',
       theme: ThemeData(
         brightness: Brightness.light,
-        primarySwatch: Colors.grey,
+        primarySwatch: createMaterialColor(
+          Color(0xff2D6E85),
+        ),
         bottomSheetTheme:
             BottomSheetThemeData(backgroundColor: Colors.black.withOpacity(0)),
       ),
